16f356ffb13a2062ca168f17d5af5b1f92d4e4fd
Chris Watson <chris@Chriss-MacBook-Air.local>
2020-03-14 11:34:08 +0000

    Auto scroll photo gallery

--- SOSGas/wwwroot/js/Slideshow.js
+++ SOSGas/wwwroot/js/Slideshow.js
@@ -1,29 +1,34 @@
-﻿var slideIndex = 1;
-showSlides(slideIndex);
+﻿$(document).ready(function () {
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+    var slideIndex = 1;
+    showSlides(slideIndex);
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+    setInterval(function () {
+        showSlides(slideIndex);
+        slideIndex = slideIndex + 1;
+    }, 2500);
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-    dots[slideIndex - 1].className += " active"
-    console.log(dots[slideIndex - 1].className);
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
 
-    // TODO: Add Auto scroll;
-}
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        if (n > slides.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = slides.length }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active"
+    }
+});
